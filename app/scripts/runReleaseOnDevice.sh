ec5ca79b0bf6c841ee5d17a566c9fc92523495c2
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2023-06-20 22:37:48 +0200

    update script

--- app/scripts/runReleaseOnDevice.sh
+++ app/scripts/runReleaseOnDevice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "${0%/*}" # directory of the script
-cd ..        # root directory of the project
+cd ..        # app directory of the project
 
 #adb kill-server
 #adb start-server
@@ -19,19 +19,8 @@ adb get-state 1>/dev/null 2>&1 && printf "\033[32mDEVICE ATTACHED\033[0m\n\n" ||
 adb shell pm list packages | grep com.in.my.district && adb uninstall com.in.my.district
 
 cordova clean
+ls "../keys-configs/$KEY_FILENAME"
 
-cordova build --release android -- --packageType=apk
+printf '\n\n\n\n\n'
 
-cp ../keys-configs/$KEY_FILENAME platforms/android/app/build/outputs/apk/release/
-cd platforms/android/app/build/outputs/apk/release/
-
-# old method of signing; after Android 11 one must use apksigner instead
-# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEY_FILENAME -storepass $KEY_PASS app-release-unsigned.apk $KEY_ALIAS
-
-zipalign -v 4 app-release-unsigned.apk inMyDistrict.apk
-apksigner sign --ks $KEY_FILENAME --pass-encoding utf-8 --ks-key-alias $KEY_ALIAS --ks-pass pass:$KEY_PASS --key-pass pass:$KEY_PASS inMyDistrict.apk
-
-
-cd ../../../../../../..
-
-cordova run android --device --release --noprepare --nobuild
+cordova run android --release -- --keystore="../keys-configs/$KEY_FILENAME" --storePassword=$KEY_PASS --alias=$KEY_ALIAS --password=$KEY_PASS --packageType=apk
